cbf8b9541cb3a243c53fd14a4646d7f40b3c27a0
Hridayansh018 <hridayansh018@gmail.com>
2024-04-23 23:53:52 +0530

    Update

---
+++ LinckedList/linckedlist.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+#CREATING NODES
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+#CONNECTING NODES
+node1.next = node2
+node2.next = node3
+node3.next = node4
+#LINCKED LIST IS READY
