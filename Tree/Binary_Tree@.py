e54df98fc57be19ae0c82f0ced933f9e74e50cff
Hridayansh018 <hridayansh018@gmail.com>
2024-05-09 01:00:51 +0530

    Insertion update

--- Tree/Binary_Tree@.py
+++ Tree/Binary_Tree@.py
@@ -1,6 +1,18 @@
 class TreeNode:
-    def __init__(self,data,left,right):
-        self.data = data
+    def __init__(self,value,left,right):
+        self.data = value
         self.left = None
         self.right = None
-        
+
+class Tree:
+    def create(self,data):
+        return TreeNode(data)
+    
+    def insert(self,Node,data):
+        if Node is None:
+            return self.create(data)
+        if data < Node.data:
+            Node.left = self.insert(Node.left,data)
+        else:
+            Node.right = self.insert(Node.right,data)
+        return Node
